7ce2b8a87f601081542cdf0ff5a5600e2ea1ccfe
adameli22 <adameli22@gmail.com>
2020-03-27 20:22:53 +0000

    fifth commit

--- test_Calculator.py
+++ test_Calculator.py
@@ -3,14 +3,23 @@ def add(string):
         return 0
 
     else:
-        # sum_num = 0
+        minus_lis = []
         string_lis = string.split(',')
         int_string = [int(x) for x in string_lis]
-        return sum(int_string)
+        for i in int_string:
+            if i > 1000:
+                int_string.remove(i)
+
+            if i < 0:
+                minus_lis.append(i)
+
+        if len(minus_lis) == 0:
+            return sum(int_string)
+        
+        else:
+            return "Negatives not allowed:"+str(minus_lis)[1:-1]
+
          
-
-                
-
 
 def test_inputs():
     assert add("") == 0
@@ -18,5 +27,11 @@ def test_inputs():
     assert add("1,2") == 3
     assert add("1,2,3,4,5") == 15
     assert add("10,2,5,22,1,1") == 41
+    assert add("1001,2") == 2
+    assert add("-1,2") ==  "Negatives not allowed:-1"
+    assert add("2,-4,3,-5") ==  "Negatives not allowed:-4, -5"
+
+
+
     
 
