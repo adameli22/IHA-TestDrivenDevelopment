bc77114dde3bb67840a30b7a0226b9dd0771feef
adameli22 <adameli22@gmail.com>
2020-03-27 18:07:44 +0000

    third commit

--- test_Calculator.py
+++ test_Calculator.py
@@ -18,11 +18,10 @@ def add(num):
 
 
 
-# print(add(""))
-# print(add("1"))
-# print(add("1,2"))
-
 def test_inputs():
     assert add("") == 0
     assert add("1") == 1
     assert add("1,2") == 3
+    assert add("1,2,3,4,5") == 15
+    
+
